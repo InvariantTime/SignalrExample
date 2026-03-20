708202541ee804d7f0f082759a657867ed9b9094
InvariantTime <diod.vt@gmail.com>
2026-03-20 23:07:17 +0300

    Добавьте файлы проекта.

---
+++ SignalrExample/Hubs/TranslationHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalrExample.Hubs;
+
+
+public interface ITranslationHubClient
+{
+    Task OnObjectChangedAsync();
+}
+
+public class TranslationHub : Hub<ITranslationHubClient>
+{
+}
